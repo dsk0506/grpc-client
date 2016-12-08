56667e5931a1b55fc72630235a6821ba5c48aa9c
dingshoukun <dingshoukun@quancheng-ec.com>
2016-12-08 17:52:43 +0800

    grpc client

---
+++ src/grpc/client/Client.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dsk
+ * Date: 2016/12/8
+ * Time: 下午5:50
+ */
+
+namespace grpc\client;
+
+
+class Client
+{
+
+    public function test(){
+        echo "just for test";
+    }
+}
